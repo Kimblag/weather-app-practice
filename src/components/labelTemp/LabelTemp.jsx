322d208108bb0800353dbd4dc1a47220ad08184a
Kimberly Blandon <kimberly.blandong@gmail.com>
2022-02-25 17:07:35 -0300

    add styles

--- src/components/labelTemp/LabelTemp.jsx
+++ src/components/labelTemp/LabelTemp.jsx
@@ -1,7 +1,9 @@
+import styles from "./LabelTemp.module.css"
+
 //para no repetir codigo arriba creo componente que reutilizo.
 export default function LabelTemp ({label, value}){
     return (
-    <div>
+    <div className={styles.labelTemp}>
     <label>{label}</label>
     <span>{value}</span>
   </div>
